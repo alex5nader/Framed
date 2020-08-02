f855f534097c0dc1e41ab6ee152c7960b84e5c4c
Alex Habich <me@alexnader.dev>
2020-08-02 16:47:12 -0500

    Add recipes

---
+++ src/main/resources/data/framity/recipes/stonecutting/gen_recipes.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+
+const sources = [
+    "block_frame"
+];
+const results = [
+    // ["door_frame"],
+    ["fence_frame", 1],
+    ["fence_gate_frame", 1],
+    ["path_frame", 1],
+    ["slab_frame", 2],
+    ["stairs_frame", 1],
+    // ["torch_frame"],
+    // ["trapdoor_frame"],
+];
+
+sources.forEach(source => {
+    results.forEach(([result, count]) => {
+        let name = source + "_to_" + result + ".json";
+
+        let recipe = {
+            type: "minecraft:stonecutting",
+            ingredient: {
+                item: "framity:" + source,
+            },
+            result: "framity:" + result,
+            count,
+        }
+
+        fs.writeFileSync(name, JSON.stringify(recipe))
+    })
+})
