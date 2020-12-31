11ccc23dcaf0a6cdbe9771a8c64c80f668ed420a
Alex Habich <me@alexnader.dev>
2020-12-31 16:35:31 -0600

    Update deps

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -33,7 +33,7 @@ object Fabric {
 
     val yarn = Dependency("net.fabricmc", "yarn", "1.16.4+build.7:v2")
     val loader = Dependency("net.fabricmc", "fabric-loader", "0.10.8", referenceOnCurseforge = false)
-    val api = Dependency("net.fabricmc.fabric-api", "fabric-api", "0.28.4+1.16")
+    val api = Dependency("net.fabricmc.fabric-api", "fabric-api", "0.29.0+1.16")
 }
 
 object Framed : Dependency("dev.alexnader", "framed", "1.0") {
@@ -50,9 +50,9 @@ val Jsr305 = Dependency("com.google.code.findbugs", "jsr305", "3.0.2")
 
 val LibGui = Dependency("io.github.cottonmc", "LibGui", "3.3.2+${Minecraft.version}", curseforgeSlug = "libgui")
 
-val Frex = Dependency("grondag", "frex-${Minecraft.tag}", "4.2.183", transitive = false, curseforgeSlug = "frex")
-val Jmx = Dependency("grondag", "jmx-${Minecraft.tag}", "1.19.168", transitive = false, curseforgeSlug = "jmx")
-val Canvas = Dependency("grondag", "canvas-${Minecraft.tag}", "1.0.1262", curseforgeSlug = "canvas-renderer")
+val Frex = Dependency("grondag", "frex-${Minecraft.tag}", "4.4.199", transitive = false, curseforgeSlug = "frex")
+val Jmx = Dependency("grondag", "jmx-${Minecraft.tag}", "1.19.171", transitive = false, curseforgeSlug = "jmx")
+val Canvas = Dependency("grondag", "canvas-${Minecraft.tag}", "1.0.1263", curseforgeSlug = "canvas-renderer")
 
 val Couplings = CurseMavenDependency("couplings", 395659, 3111745)
 
